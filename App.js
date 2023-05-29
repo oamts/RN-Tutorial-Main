af59e9c6f0a497bd2b63ef0432309dc03a47eed8
mts-almeida <mateusoliveiraalmeida@gmail.com>
2023-05-29 20:33:23 -0300

    RN-Tutorial-06

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button, Linking} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Mash');
+  const [session, setSession] = useState({number: 6, title: 'state'});
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName('Programming with Mash');
+    setSession({number: 7, title: 'Style'});
+    setCurrent(false);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming with Mash</Text>
-      <Button
-        title="youtube channel"
-        onPress={() => {
-          Linking.openURL('https://youtube.com/programmingwithmash');
-        }}
-      />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>
+        This is session number {session.number} and about {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'current session' : 'next session'}
+      </Text>
+      <Button title="Update State" onPress={onClickHandler} />
     </View>
   );
 };
